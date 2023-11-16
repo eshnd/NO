05f0b8530317a6768a3e7a9cead47207498290ad
etch <etchover@gmail.com>
2023-11-16 16:01:41 -0600

    added sfx and stuff

--- amorgus.gd
+++ amorgus.gd
@@ -13,18 +13,22 @@ func _process(delta):
 	if sfxb == false:
 		$"../Amoogus".scale = Vector2.ZERO
 	if spam == true:
-		text = text + "poopapoop"
+		if $"../shimmy".has_stream_playback() == false:
+			$"../shimmy".play()
+		text = text + " shimmy shimmy ay shimmy ay shimmy ya drive falalallaa drive falaalalala"
+	else:
+		$"../shimmy".stop()
 		
 func _ready():
 	$"../Bad".scale = Vector2.ZERO
 	text = "" 
 
 func _on_button_pressed():
-	text = text + "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start."
+	text = text + " The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start. "
 	$"../fitness".play()
 
 func _on_button_2_pressed():
-	text = text + "amogus :O"
+	text = text + " amogus :O "
 	$"../sfx".play()
 	sfxb = true
 
@@ -37,4 +41,3 @@ func _on_button_4_pressed():
 		spam = false
 	else:
 		spam = true
-	
